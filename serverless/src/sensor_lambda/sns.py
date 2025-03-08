fbe4a3b9adbf41f37d5cebc6cc507b167c953a4c
Krzysztof Zajac <kzajac1997@gmail.com>
2025-03-08 18:22:58 +0100

    [serverless](feat) Add infra configuration

--- serverless/src/sensor_lambda/sns.py
+++ serverless/src/sensor_lambda/sns.py
@@ -6,7 +6,7 @@ import boto3
 def notify(message: str):
     client = boto3.client("sns")
     client.publish(
-        TopicArn=os.getenv("SNS_ARN"),
+        TopicArn=os.getenv("SNS_TOPIC_ARN"),
         Message=message,
         Subject="AWS Lab Error Alert",
     )
