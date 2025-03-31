77b0227326577e984675d84f462a82ff6c97016f
Krzysztof Zajac <kzajac1997@gmail.com>
2025-03-31 20:27:15 +0200

    [serverless](feat) Add S3 bucket for passing SFN payloads

--- serverless/infra/s3.tf
+++ serverless/infra/s3.tf
@@ -1,3 +1,24 @@
 resource "aws_s3_bucket" "sensor_data" {
   bucket = "sfn-sensor-analytics"
 }
+
+resource "aws_s3_bucket" "sfn_payloads" {
+  bucket = "sfn-payloads"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example_lifecycle_policy" {
+  bucket = aws_s3_bucket.sfn_payloads.id
+
+  rule {
+    id     = "expire-after-1-day"
+    status = "Enabled"
+
+    expiration {
+      days = 1
+    }
+
+    filter {
+    prefix = "" # Applies to all objects in the bucket
+  }
+  }
+}
