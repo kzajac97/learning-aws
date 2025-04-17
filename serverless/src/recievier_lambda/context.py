0fd04d59caa744052e7d291f0aacf0f6d9512bf1
Krzysztof Zajac <kzajac1997@gmail.com>
2025-04-17 08:30:26 +0200

    [main](feat) Make context region and profile aware -> enable local development better

--- serverless/src/recievier_lambda/context.py
+++ serverless/src/recievier_lambda/context.py
@@ -10,15 +10,24 @@ class Context:
     input_bucket: str
     payload_bucket: str
 
+    aws_profile: str
+    aws_region: str
+
     sqs_client: Any
     s3_client: Any
 
     @classmethod
     def from_dict(cls, env: dict) -> Self:
+        aws_profile = env["AWS_PROFILE_NAME"]
+        aws_region = env["AWS_REGION"]
+        session = boto3.Session(profile_name=aws_profile, region_name=aws_region)
+
         return cls(
             sqs_url=env["SQS_URL"],
             input_bucket=env["INPUT_BUCKET"],
             payload_bucket=env["PAYLOAD_BUCKET"],
-            sqs_client=boto3.client("sqs"),
-            s3_client=boto3.client("s3"),
+            aws_profile=aws_profile,
+            aws_region=aws_region,
+            sqs_client=session.client("sqs"),
+            s3_client=session.client("s3"),
         )
