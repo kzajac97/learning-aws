b21c67b3688d9697bd5e5dbc849377a031229d3b
Krzysztof Zajac <kzajac1997@gmail.com>
2025-04-17 08:30:34 +0200

    [main](feat) Adjust SQS settings

--- serverless/infra/sqs.tf
+++ serverless/infra/sqs.tf
@@ -1,9 +1,9 @@
 locals {
-  report_frequency_min = 5
+  report_frequency_min = 60
 }
 
 resource "aws_sqs_queue" "sensor_queue" {
   name                       = "sensor_queue"
-  visibility_timeout_seconds = local.report_frequency_min * 60
-  message_retention_seconds  = 1 * 60 * 60 # 1 hour
+  visibility_timeout_seconds = 0
+  message_retention_seconds  = local.report_frequency_min
 }
