0fcc74d0b78e2763b59907a5c708c0329b553b78
Krzysztof Zajac <kzajac1997@gmail.com>
2025-04-22 14:00:55 +0200

    [data_processing](chore) Move glue job source code to directory

--- data_processing/infra/ingest.tf
+++ data_processing/infra/ingest.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_object" "glue_etl_script_source" {
   bucket = aws_s3_bucket.glue_assets.bucket
   key    = "${aws_s3_object.glue_scripts.key}ingest.py" # trailing / is part of aws_s3_object.glue_scripts.key
-  source = "${path.module}/../src/ingest.py"
+  source = "${path.module}/../src/glue/ingest.py"
 
-  source_hash = filemd5("${path.module}/../src/ingest.py")
+  source_hash = filemd5("${path.module}/../src/glue/ingest.py")
 
   lifecycle {
     create_before_destroy = true
