63f413c07416767450b451a37903454479c75539
Krzysztof Zajac <kzajac1997@gmail.com>
2025-04-28 15:36:26 +0200

    [data_processing](feat) Add DB configuration to SSM

--- data_processing/infra/database.tf
+++ data_processing/infra/database.tf
@@ -1,7 +1,31 @@
+variable "db_name" {
+  type        = string
+  description = "RDS database name"
+  default     = "survey"
+}
+
+variable "db_username" {
+  type        = string
+  description = "RDS username"
+  default     = "dbuser"
+}
+
 variable "password" {
   type        = string
   description = "RDS password (secret)"
   sensitive   = true
+}
+
+resource "aws_ssm_parameter" "db_name" {
+  name = "/db/name"
+  type = "String"
+  value = var.db_name
+}
+
+resource "aws_ssm_parameter" "db_user" {
+  name = "/db/user"
+  type = "String"
+  value = var.db_username
 }
 
 resource "aws_ssm_parameter" "db_password" {
@@ -12,7 +36,7 @@ resource "aws_ssm_parameter" "db_password" {
 
 resource "aws_db_instance" "postgres" {
   identifier     = "survey-postgres"
-  db_name        = "survey"
+  db_name        = var.db_name
   engine         = "postgres"
   engine_version = "17.2"
 
@@ -21,11 +45,23 @@ resource "aws_db_instance" "postgres" {
   allocated_storage = 10
   storage_type      = "gp2"
 
-  username = "dbuser"
-  password = aws_ssm_parameter.db_password.value
+  username = var.db_username
+  password = var.password
 
   skip_final_snapshot = true
   publicly_accessible = true
 
   vpc_security_group_ids = [aws_security_group.rds_restricted.id]
+}
+
+resource "aws_ssm_parameter" "db_host" {
+  name  = "/db/host"
+  type  = "SecureString"
+  value = aws_db_instance.postgres.endpoint
+}
+
+resource "aws_ssm_parameter" "db_port" {
+  name  = "/db/port"
+  type  = "String"
+  value = aws_db_instance.postgres.port
 }
