30089995c5807f6359ab592c8c20beb9cbbf7b75
Krzysztof Zajac <kzajac1997@gmail.com>
2025-04-29 18:27:21 +0200

    [data_processing](feat) Implement ETL for data normalization

---
+++ data_processing/infra/transform.tf
@@ -0,0 +1,40 @@
+locals {
+  local_transform_script_path = "${path.module}/../src/glue/transform.py"
+}
+
+resource "aws_s3_object" "glue_transform_script_source" {
+  bucket = aws_s3_bucket.glue_assets.bucket
+  key    = "${var.scripts_directory}/transform.py"
+  source = local.local_transform_script_path
+
+  source_hash = filemd5(local.local_transform_script_path)
+}
+
+resource "aws_glue_job" "glue_transform" {
+  name     = "dps-transform"
+  role_arn = data.aws_iam_role.main_role.arn
+
+  worker_type       = "G.1X"
+  number_of_workers = 2
+
+  max_retries  = 1
+  timeout      = 10
+  glue_version = "5.0"
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${aws_s3_object.glue_scripts.bucket}/${aws_s3_object.glue_transform_script_source.key}"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--enable-metrics"                   = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--TempDir"                          = "s3://${aws_s3_object.sparklogs.bucket}/${aws_s3_object.sparklogs.key}"
+    "--JOB_NAME"                         = "dps-transform"
+    "--glue_database"                    = aws_glue_catalog_database.data_processing_db.name
+    "--input_glue_table_name"            = aws_glue_catalog_table.processed_glue_table.name
+    "--output_s3_dir"                    = "s3://${aws_s3_bucket.data.bucket}/${var.normalized_data_directory}/"
+  }
+}
+
