ce841de6d81b352c7ba6fc2e362125358e9b0077
Krzysztof Zajac <kzajac1997@gmail.com>
2025-05-05 21:37:11 +0200

    [serverless](chore) Use remote backend for serverless

--- serverless/infra/main.tf
+++ serverless/infra/main.tf
@@ -9,3 +9,24 @@ provider "archive" {}
 data "aws_iam_role" "main_role" {
   name = "LabRole"
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "serverless-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "infra-shared-tf-backend" # bucket created manually, shared between applications
+    key            = "serverless.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "serverless-tf-locks"
+    encrypt        = true
+  }
+}
