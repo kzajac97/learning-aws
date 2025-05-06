deabf618a474809c3d3acc66dc138e6bc32c3579
Krzysztof Zajac <kzajac1997@gmail.com>
2025-05-06 14:51:24 +0200

    [serverless](chore) Use remote backend for data_processing

--- data_processing/infra/main.tf
+++ data_processing/infra/main.tf
@@ -9,3 +9,24 @@ provider "archive" {}
 data "aws_iam_role" "main_role" {
   name = "LabRole"
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "data-processing-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "infra-shared-tf-backend" # bucket created manually, shared between applications
+    key            = "data-processing.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "data-processing-tf-locks"
+    encrypt        = true
+  }
+}
