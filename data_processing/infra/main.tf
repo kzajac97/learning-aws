46b7becef6a818c6087c9af31829d32e1860b05e
Krzysztof Zajac <kzajac1997@gmail.com>
2025-05-06 18:52:20 +0200

    [main](fix) Fix linters

--- data_processing/infra/main.tf
+++ data_processing/infra/main.tf
@@ -12,10 +12,10 @@ data "aws_iam_role" "main_role" {
 
 terraform {
   backend "s3" {
-    bucket = "infra-shared-tf-backend" # bucket created manually, shared between applications
-    key    = "data-processing.tfstate"
-    region = "us-east-1"
+    bucket         = "infra-shared-tf-backend" # bucket created manually, shared between applications
+    key            = "data-processing.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
-    encrypt = true
+    encrypt        = true
   }
 }
