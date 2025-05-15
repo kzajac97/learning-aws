90ceff18860331d0596f67689000c67e92228a4a
Krzysztof Zajac <kzajac1997@gmail.com>
2025-05-15 13:25:55 +0200

    [main](feat) Use the Lambda module in application infrastructure code

--- serverless/infra/analytics.tf
+++ serverless/infra/analytics.tf
@@ -1,58 +1,40 @@
-data "archive_file" "receiver_lambda_source_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/recievier_lambda"
-  output_path = "${path.module}/.src/recievier_lambda.zip"
-}
+module "receiver_lambda" {
+  source = "./modules/lambda"
 
-resource "aws_lambda_function" "receiver_lambda" {
-  filename                       = data.archive_file.receiver_lambda_source_code.output_path
-  function_name                  = "sensor-analytics-receiver"
-  role                           = data.aws_iam_role.main_role.arn
-  handler                        = "main.handler"
-  runtime                        = "python3.13"
-  timeout                        = 60
-  memory_size                    = 1024
-  source_code_hash               = filebase64sha256(data.archive_file.receiver_lambda_source_code.output_path)
-  reserved_concurrent_executions = 1
+  env              = "dev"
+  source_code_path = "${path.module}/../src/receiver_lambda"
+  role_arn         = data.aws_iam_role.main_role.arn
 
-  # so far hardcoded layer ARN for pandas
-  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python313:1"]
+  handler     = "main.handler"
+  runtime     = "python3.13"
+  timeout     = 60
+  memory_size = 1024
 
-  environment {
-    variables = {
-      ENV            = "dev"
-      SQS_URL        = aws_sqs_queue.sensor_queue.id
-      INPUT_BUCKET   = aws_s3_bucket.sensor_data.bucket
-      PAYLOAD_BUCKET = aws_s3_bucket.sfn_payloads.bucket
-    }
+  max_parallel_executions = 1
+
+  env_variables = {
+    SQS_URL        = aws_sqs_queue.sensor_queue.id
+    INPUT_BUCKET   = aws_s3_bucket.sensor_data.bucket
+    PAYLOAD_BUCKET = aws_s3_bucket.sfn_payloads.bucket
   }
 }
 
-data "archive_file" "reporter_lambda_source_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/reporter_lambda"
-  output_path = "${path.module}/.src/reporter_lambda.zip"
-}
+module "reporter_lambda" {
+  source = "./modules/lambda"
 
-resource "aws_lambda_function" "reporter_lambda" {
-  filename                       = data.archive_file.reporter_lambda_source_code.output_path
-  function_name                  = "sensor-analytics-batch-reporter"
-  role                           = data.aws_iam_role.main_role.arn
-  handler                        = "main.handler"
-  runtime                        = "python3.13"
-  timeout                        = 60
-  memory_size                    = 1024
-  source_code_hash               = filebase64sha256(data.archive_file.reporter_lambda_source_code.output_path)
-  reserved_concurrent_executions = 8
+  env              = "dev"
+  source_code_path = "${path.module}/../src/reporter_lambda"
+  role_arn         = data.aws_iam_role.main_role.arn
 
-  # so far hardcoded layer ARN for pandas
-  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python313:1"]
+  handler     = "main.handler"
+  runtime     = "python3.13"
+  timeout     = 60
+  memory_size = 1024
 
-  environment {
-    variables = {
-      ENV            = "dev"
-      PAYLOAD_BUCKET = aws_s3_bucket.sfn_payloads.bucket
-    }
+  max_parallel_executions = 8
+
+  env_variables = {
+    PAYLOAD_BUCKET = aws_s3_bucket.sfn_payloads.bucket
   }
 }
 
@@ -61,7 +43,7 @@ resource "aws_sfn_state_machine" "sensor_analytics_workflow" {
   role_arn = data.aws_iam_role.main_role.arn
 
   definition = templatefile("${path.module}/sensor-analytics-workflow.asl.json", {
-    receiver_lambda_arn = "${aws_lambda_function.receiver_lambda.arn}:$LATEST"
-    reporter_lambda_arn = "${aws_lambda_function.reporter_lambda.arn}:$LATEST"
+    receiver_lambda_arn = "${module.receiver_lambda.function.arn}:$LATEST"
+    reporter_lambda_arn = "${module.reporter_lambda.function.arn}:$LATEST"
   })
 }
--- serverless/infra/sensor.tf
+++ serverless/infra/sensor.tf
@@ -1,43 +1,20 @@
-data "archive_file" "lambda_source_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/sensor_lambda"
-  output_path = "${path.module}/.src/sensor_lambda.zip"
-}
-
 resource "aws_sns_topic" "sensor_notifications" {
   name = "sensor_notifications"
 }
 
-resource "aws_lambda_function" "sensor_lambda" {
-  filename                       = data.archive_file.lambda_source_code.output_path
-  function_name                  = "sensor-lambda"
-  role                           = data.aws_iam_role.main_role.arn
-  handler                        = "main.lambda_handler"
-  runtime                        = "python3.13"
-  timeout                        = 10
-  source_code_hash               = filebase64sha256(data.archive_file.lambda_source_code.output_path)
-  reserved_concurrent_executions = 4
+module "sensor_lambda" {
+  source = "./modules/lambda"
 
-  environment {
-    variables = {
-      ENV                   = "dev"
-      SNS_TOPIC_ARN         = aws_sns_topic.sensor_notifications.arn
-      SENSOR_REGISTRY_TABLE = aws_dynamodb_table.sensor_registry.name
-      SQS_URL               = aws_sqs_queue.sensor_queue.id
-    }
+  env              = "dev"
+  source_code_path = "${path.module}/../src/sensor_lambda"
+  role_arn         = data.aws_iam_role.main_role.arn
+
+  handler = "main.lambda_handler"
+  runtime = "python3.13"
+
+  env_variables = {
+    SNS_TOPIC_ARN         = aws_sns_topic.sensor_notifications.arn
+    SENSOR_REGISTRY_TABLE = aws_dynamodb_table.sensor_registry.name
+    SQS_URL               = aws_sqs_queue.sensor_queue.id
   }
-}
-
-resource "aws_lambda_permission" "allow_sns" {
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.sensor_lambda.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.sensor_notifications.arn
-}
-
-resource "aws_sns_topic_subscription" "lambda_subscription" {
-  topic_arn = aws_sns_topic.sensor_notifications.arn
-  protocol  = "lambda"
-  endpoint  = aws_lambda_function.sensor_lambda.arn
 }
