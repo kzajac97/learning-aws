def51a9d90a1e69e4c3ece24f334b5d9834d6f49
Krzysztof Zajac <kzajac1997@gmail.com>
2025-06-12 17:54:07 +0200

    [serverless](tests) Properly test DynamoDB usage in sensor_lambda

--- serverless/tests/test_sensor_lambda.py
+++ serverless/tests/test_sensor_lambda.py
@@ -8,6 +8,36 @@ import pytest
 from tests import settings
 
 
+def prepare_dynamodb(dynamodb, table_name: str, content: list[dict]):
+    """Prepare the mocked sensor_registry DynamoDB table with the given content."""
+    for item in content:
+        if content:
+            pk = item.get("sensor_id")
+            ok = item.get("working_ok")
+            dynamodb.put_item(TableName=table_name, Item={"sensor_id": {"S": pk}, "working_ok": {"BOOL": ok}})
+
+
+def assert_dynamodb(dynamodb, table_name: str, expected_content: list[dict]):
+    """Assert that the mocked sensor_registry DynamoDB table contains the expected content."""
+    items = []
+    exclusive_start_key = None
+
+    while True:
+        scan_kwargs = {"TableName": table_name}
+        if exclusive_start_key:
+            scan_kwargs["ExclusiveStartKey"] = exclusive_start_key
+
+        response = dynamodb.scan(**scan_kwargs)
+        for item in response.get("Items", []):
+            items.append(item)
+
+        exclusive_start_key = response.get("LastEvaluatedKey")
+        if not exclusive_start_key:
+            break
+
+    assert items == expected_content
+
+
 @pytest.fixture(scope="function")
 def mock_env(mocked_dynamodb, mocked_sns, mocked_sqs):
     _, queue_url = mocked_sqs
@@ -35,15 +65,57 @@ def mock_env(mocked_dynamodb, mocked_sns, mocked_sqs):
         "expected_sqs_messages",
     ],
     (
+        # test case 1: temperature in range with new sensor
         (
             {"sensor_id": "1", "location_id": "A", "value": 2000},  # event
             {},  # empty dynamodb_content
             200,
             {"sensor_id": "1", "location_id": "A", "status": "OK", "timestamp": settings.TIME},
-            {},  # expected_dynamodb_content
+            [{"sensor_id": {"S": "1"}, "working_ok": {"BOOL": True}}],  # expected_dynamodb_content
             [],  # expected_sns_messages
             [],  # expected_sqs_messages
         ),
+        # test case 2: temperature in range with existing sensor
+        (
+            {"sensor_id": "2", "location_id": "A", "value": 1500},  # event
+            [{"sensor_id": "2", "working_ok": True}],  # dynamodb_content
+            200,
+            {"sensor_id": "2", "location_id": "A", "status": "OK", "timestamp": settings.TIME},
+            [{"sensor_id": {"S": "2"}, "working_ok": {"BOOL": True}}],  # expected_dynamodb_content
+            [],  # expected_sns_messages
+            [],  # expected_sqs_messages
+        ),
+        # test case 3: temperature in range with broken sensor
+        (
+            {"sensor_id": "3", "location_id": "A", "value": 1800},  # event
+            [{"sensor_id": "3", "working_ok": False}],  # dynamodb_content
+            204,
+            {},  # no response body
+            [{"sensor_id": {"S": "3"}, "working_ok": {"BOOL": False}}],  # expected_dynamodb_content
+            [],  # expected_sns_messages
+            [],  # expected_sqs_messages
+        ),
+        # test case 4: temperature out of range with new sensor
+        (
+            {"sensor_id": "4", "location_id": "A", "value": 25_000},  # event
+            {},  # empty dynamodb
+            204,
+            {},  # no response body
+            [{"sensor_id": {"S": "4"}, "working_ok": {"BOOL": False}}],  # expected_dynamodb_content
+            [],  # expected_sns_messages
+            [],  # expected_sqs_messages
+        ),
+        # test case 5: temperature out of range with existing sensor
+        (
+            {"sensor_id": "5", "location_id": "A", "value": 25_000},  # event
+            [{"sensor_id": "5", "working_ok": True}],  # dynamodb_content with existing sensor - initially working
+            204,
+            {},  # no response body
+            # expected_dynamodb_content -> sensor is now marked as broken
+            [{"sensor_id": {"S": "5"}, "working_ok": {"BOOL": False}}],
+            [],  # expected_sns_messages
+            [],  # expected_sqs_messages
+        )
     ),
 )
 @freezegun.freeze_time(settings.TIME)
@@ -56,7 +128,6 @@ def test_sensor_lambda(
     expected_sns_messages,
     expected_sqs_messages,
     mock_env,
-    lambda_context,
     mocked_s3,
     mocked_dynamodb,
     mocked_sns,
@@ -64,11 +135,18 @@ def test_sensor_lambda(
     mocker,
 ):
     mocker.patch.dict(os.environ, mock_env, clear=True)  # patch environment variables before importing handler
+    prepare_dynamodb(mocked_dynamodb, settings.SENSOR_REGISTRY_TABLE, dynamodb_content)
+
     from src.sensor_lambda.main import main
 
     response = main(event)
-    body = json.loads(response.get("body"))
-    temperature = body.pop("temperature")  # do not assert exact quality on temperature  # noqa
+
+    body = json.loads(response.get("body", "{}"))
+
+    if body:
+        temperature = body.pop("temperature")  # do not assert exact quality on temperature  # noqa
 
     assert response["status_code"] == expected_status_code
     assert body == expected_response
+    # assert AWS resources behave as expected
+    assert_dynamodb(mocked_dynamodb, settings.SENSOR_REGISTRY_TABLE, expected_dynamodb_content)
